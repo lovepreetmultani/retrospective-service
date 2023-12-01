8d807b53295af9b2a10c79c5b2b583997ec4c7ff
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-12-01 12:40:33 +0000

    Initial commit

---
+++ src/main/java/sis_tv/retrospectiveservice/RetrospectiveServiceApplication.java
@@ -0,0 +1,13 @@
+package sis_tv.retrospectiveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospectiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrospectiveServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sis_tv/retrospectiveservice/RetrospectiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sis_tv.retrospectiveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrospectiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
