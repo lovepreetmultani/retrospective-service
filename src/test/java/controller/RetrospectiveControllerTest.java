fa97c68bcb979e5ffd8de77f5c3c0518e18186fd
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-12-27 10:56:30 +0000

    library update

--- src/test/java/controller/RetrospectiveControllerTest.java
+++ src/test/java/controller/RetrospectiveControllerTest.java
@@ -1,4 +1,5 @@
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 import retrospectiveservice.controller.RetrospectiveController;
 
